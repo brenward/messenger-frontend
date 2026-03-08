155853b965821037af20185a6c8d6ca243e0fc49
brenward <bren.ward@gmail.com>
2026-03-08 17:50:01 +0000

    #Setting username

--- src/app/usercomponent/usercomponent.ts
+++ src/app/usercomponent/usercomponent.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgIf } from "../../../node_modules/@angular/common/types/_common_module-chunk";
 
 @Component({
   selector: 'app-usercomponent',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './usercomponent.html',
   styleUrl: './usercomponent.css',
 })
-export class Usercomponent {}
+export class Usercomponent {
+  username:string = "";
+  userSet:boolean = false; 
+
+  onUserNameEntered(nameInput:HTMLInputElement){
+    this.username = nameInput.value;
+    this.userSet = true;
+  }
+}
