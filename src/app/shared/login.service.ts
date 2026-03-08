eb7dba96c4c4dc85e128a57dded38a17fe865165
brenward <bren.ward@gmail.com>
2026-03-08 18:33:36 +0000

    #Setting up http call

--- src/app/shared/login.service.ts
+++ src/app/shared/login.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from "@angular/core";
 import { User } from "./user.model";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class LoginService{
     currentUser:User | undefined;
 
+    constructor(private http:HttpClient){}
+
     setUser(username:string){
+        this.lookupUserOnService(username).subscribe({
+            next: (response) => {this.setCurrentUser(username)},
+            error: (error) => { console.log("error")}
+        });        
+    }
+
+    setCurrentUser(username:string){
         this.currentUser = new User(username, "");
     }
 
     getUser(){
         return this.currentUser;
+    }
+
+    lookupUserOnService(username:string){
+        return this.http.get("http://localhost:8181/chats/" + username);       
     }
 
 
